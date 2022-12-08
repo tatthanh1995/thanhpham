6aaaece0155082d4f5ccab62b6626ea4a8395df5
tatthanh1995 <119849608+tatthanh1995@users.noreply.github.com>
2022-12-08 17:26:36 +0700

    NhungJS

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
 function hello() {
     alert("Xin chào!");
 }
+function xinchao() {
+    document.getElementById("hi").innerHTML = "Chào bạn!"
+}
+function bye() {
+    document.write("Goodbye!")
+}
+function goodmorning() {
+    console.log("chào buổi chiều!")
+}
